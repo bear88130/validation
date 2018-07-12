867fa42d6eca75aba77d5bb3a8ec4b34f83d41a8
bear88130 <bear88130@yahoo.com.tw>
2018-07-12 23:35:36 +0800

    先完成信箱驗證

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void {
+    // alert(/^(\w+)@([\w.]+)(\.|-)[\w.]/.test('bear@tadcon'));
+
+
+  }
+
+  checkEmail(email: string) {
+    const reEmail = new RegExp(/^(\w+)@([\w.]+)(\.|-)[\w.]/);
+    const a = email;
+    this.title = a;
+    alert(reEmail.test(email));
+  }
+
+  comfirmAccount() {
+    const reEmail = new RegExp('^(\w+)@([\w.]+)(\.|-)[\w.]');
+    alert(reEmail.test('123'));
+  }
+
 }
